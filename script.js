a1f1a80bfdcf7da9f8f2008c6ef87b4891276643
Persik-1 <118620585+Persik-1@users.noreply.github.com>
2022-12-03 17:10:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-alert("Я не успел доделать, так как очень ленивый, но я буду продолжать доделывать, мне надо будет сделать ещё css и script.js")
-function handle() {
-    alert('Форма отправлена!');
-}
 
-let button = document.querySelector('.btn');
-button.addEventListener('click', handle);
-
-function handle() {
-    let inputs = document.querySelectorAll("input");
-    let input_values = [];
-    for (let i = 0; i < inputs.length; i++) {
-        input_values.push(inputs[i].value);
-    }
-}
+let links = document.querySelectorAll('.scroll');
+let targetID;
+links.forEach(function (element){
+    element.addEventListener('click', function(event){
+        event.preventDefault();
+        targetID = element.getAttribute('href');
+        document.querySelector(targetID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+})
